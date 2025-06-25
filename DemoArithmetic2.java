911410705b4925730f45b623e966837fcad5760a
Mitali85 <mrpatil3072003@gmail.com>
2025-06-25 16:30:12 +0530

    new file added

---
+++ DemoArithmetic2.java
@@ -0,0 +1,14 @@
+class DemoArithmetic2
+{
+    public static void main(String[] args)
+{
+    int a=60;
+    int b=20;
+
+    System.out.println("Addition is"+(a+b));
+    System.out.println("Sub is"+(a-b));
+    System.out.println("Mul is"+(a*b));
+    System.out.println("Div is"+(a/b));
+    System.out.println("Reminder is"+(a%b));
+}
+}
