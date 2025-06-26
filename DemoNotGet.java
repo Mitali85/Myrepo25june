023f1f5fc361074c3e7e0b714cd8e94da7d10bcc
Mitali85 <mrpatil3072003@gmail.com>
2025-06-26 15:29:58 +0530

    Add files via upload

---
+++ DemoNotGet.java
@@ -0,0 +1,14 @@
+class DemoNotGet
+{
+public static void main(String[]args)
+{
+int p,q,r;
+p=60;
+q=50;
+r=40;
+System.out.println(!(p>q));   //(true)------false
+
+System.out.println(!(p<r));   //(false)-----true
+
+}
+}
